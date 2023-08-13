47e2466ddf78696ceeb9adecc9465667c2ba592d
Anil Devidas Janjal <132841194+AnilJanjal@users.noreply.github.com>
2023-08-13 11:51:43 +0530

    Update App.js
    add

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../styles/App.css';
 const TypesOfFruit = () => {
   return (
-    <div id="typesOfFruit">
+    <div id="typesOfFruits">
       <h2>Fruits:</h2>
       <ul>
         <li>Apples</li>
